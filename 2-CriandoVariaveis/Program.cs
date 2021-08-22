ff4fcebcea74e05154bb91ea281aec393d0527eb
Lucas Garcia <lucasugarcia@gmail.com>
2021-08-21 22:33:50 -0300

    Revisão de variáveis numéricas

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variaveis");
+
+            int idade;
+
+            idade = 10;
+            Console.WriteLine(idade);
+
+            idade = 13 + 11;
+            Console.WriteLine(idade);
+
+            idade = 4 + 4 * 2;
+            Console.WriteLine(idade);
+
+            idade = (4 + 4) * 2;
+            Console.WriteLine(idade);
+            Console.WriteLine("Sua idade é" + idade + "!");
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestePontoFlutuante/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestePontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 1250.70;
+
+            Console.WriteLine(salario);
+
+            Console.WriteLine("Meu salario é " + salario);
+
+            /*não compila
+            int valor = 12.5;*/
+
+            /*não compila
+            int valor2 = 0.0;*/
+
+            double teste = 125.50;
+
+            /*não compila
+            int valor3 = teste;*/
+
+            int divisao = 5 / 2;
+            Console.WriteLine(divisao);
+
+            double divisao2 = 5.0 / 2;
+            Console.WriteLine(divisao2);
+
+            double peso = 4.0;
+            int quantidade = 10;
+            Console.WriteLine(peso * quantidade);
+
+            Console.ReadLine();
+        }
+    }
+}
