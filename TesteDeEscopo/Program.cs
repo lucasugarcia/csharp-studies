decea78bbba59b348c88bd6aa6ae4c9513774c6f
Lucas Garcia <lucasugarcia@gmail.com>
2021-09-04 22:06:18 -0300

    Teste de escopo

---
+++ TesteDeEscopo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteDeEscopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 20;
+            int quantidadePessoas = 3;
+            bool acompanhado;
+
+            if (quantidadePessoas >= 2)
+                acompanhado = true;
+            else
+                acompanhado = false;
+
+            if (idade >= 18 || acompanhado)
+                Console.WriteLine("Seja bem vindo");
+            else
+                Console.WriteLine("infelizmente voce nao pode entrar");
+
+            Console.ReadLine();
+        }
+    }
+}
