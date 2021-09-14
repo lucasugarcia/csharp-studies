ba75b80ab0f880bd4aa5ad421841791e295a5aa2
Lucas Garcia <lucasugarcia@gmail.com>
2021-09-14 13:38:38 -0300

    Demonstração da diferença entre variável do tipo valor e variável do tipo referência

---
+++ 03-BytBank/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaLucas = new ContaCorrente();
+            contaLucas.titular = "Lucas";
+            contaLucas.agencia = 863;
+            contaLucas.numero = 863452;
+
+            ContaCorrente contaLucasSilva = new ContaCorrente();
+            contaLucasSilva.titular = "Lucas";
+            contaLucasSilva.agencia = 863;
+            contaLucasSilva.numero = 863452;
+
+            Console.WriteLine("Igualdade de tipo de referência: " + (contaLucas == contaLucasSilva));
+
+            int idade = 27;
+            int idadeMaisUmaVez = 27;
+
+            Console.WriteLine("Igualdade de tipo de valor: " + (idade == idadeMaisUmaVez));
+
+            contaLucas = contaLucasSilva;
+            Console.WriteLine(contaLucas == contaLucasSilva);
+
+            contaLucas.saldo = 300;
+            Console.WriteLine(contaLucas.saldo);
+            Console.WriteLine(contaLucasSilva.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
