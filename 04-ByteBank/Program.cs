4f25f0ff9b2150f702ff155ff31b5d76be38c8fa
Lucas Garcia <lucasugarcia@gmail.com>
2021-09-15 13:29:16 -0300

    Criando e chamando um método de uma classe

---
+++ 04-ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaLucas = new ContaCorrente();
+            contaLucas.titular = "Lucas";
+
+            Console.WriteLine("Saldo: " + contaLucas.saldo);
+            
+            bool retornoDoSaque = contaLucas.Sacar(50);
+
+            Console.WriteLine("Saldo: " + contaLucas.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
