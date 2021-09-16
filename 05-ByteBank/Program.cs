f63e0cfbc0163000745808f8678f076aa31ddd09
Lucas Garcia <lucasugarcia@gmail.com>
2021-09-16 13:21:09 -0300

    Entendendo sobre namespaces e criando a classe Cliente, para ser usada em composição com a classe ContaCorrente

---
+++ 05-ByteBank/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente titular = new Cliente();
+            titular.nome = "Lucas";
+            titular.profissao = "Desenvolvedor";
+            titular.cpf = "12332145622";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = titular;
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 563427;
+
+            Console.WriteLine(titular.nome);
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
+        }
+    }
+}
