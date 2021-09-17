a1be9ee7aead11f1359326b299b36048184f087d
Lucas Garcia <lucasugarcia@gmail.com>
2021-09-17 13:11:05 -0300

    Trabalhando com campos privados e getters e setters

---
+++ 06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        public int agencia;
+        public int numero;
+        private double _saldo = 100;
+
+        public Cliente Titular { get; set; }
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value >= 0)
+                    _saldo = value;
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+                return false;
+
+            _saldo -= valor;
+
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+                return false;
+
+            _saldo -= valor;
+
+            contaDestino.Depositar(valor);
+
+            return true;
+        }
+    }
+}
---
+++ 06-ByteBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+            cliente.nome = "Lucas";
+            cliente.cpf = "123.963.987-45";
+            cliente.profissao = "Desenvolvedor";
+
+            conta.Titular = cliente;
+
+            conta.Saldo = -10;
+
+            Console.WriteLine(conta.Saldo);
+            Console.WriteLine(conta.Titular.nome);
+
+            Console.ReadLine();
+        }
+    }
+}
