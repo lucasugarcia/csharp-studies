1f3c110a97bf716dbe2114b7ba65c5ebc3ac818c
Lucas Garcia <lucasugarcia@gmail.com>
2021-09-18 16:09:51 -0300

    Trabalhando com o construtor

---
+++ 07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+        public int Numero { get; set; }
+        public Cliente Titular { get; set; }
+
+        private int _agencia;
+        private double _saldo = 100;
+        public int Agencia
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value >= 0)
+                    _agencia = value;
+            }
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value >= 0)
+                    _saldo = value;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+                return false;
+
+            _saldo -= valor;
+
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+                return false;
+
+            _saldo -= valor;
+
+            contaDestino.Depositar(valor);
+
+            return true;
+        }
+    }
+}
