dea54578734a895847418a32016fd5f213e8c129
Lucas Garcia <lucasugarcia@gmail.com>
2021-09-27 13:52:33 -0300

    Trabalhando com sobrecarga de método

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteBank.Funcionarios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,31 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario lucas = new Funcionario();
+            lucas.Nome = "Lucas";
+            lucas.Salario = 2000;
+            lucas.CPF = "456123789-89";
+
+            gerenciador.Registrar(lucas);
+
+            Console.WriteLine(lucas.Nome);
+            Console.WriteLine(lucas.GetBonificacao());
+
+            Diretor joao = new Diretor();
+            joao.Nome = "João";
+            joao.Salario = 5000;
+            joao.CPF = "456123789-89";
+
+            gerenciador.Registrar(joao);
+
+            Console.WriteLine(joao.Nome);
+            Console.WriteLine(joao.GetBonificacao());
+
+            Console.WriteLine(gerenciador.GetTotalBonificacao());
+
+            Console.ReadLine();
         }
     }
 }
