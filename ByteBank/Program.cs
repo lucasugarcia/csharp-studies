e076dcc12468825d6d2c0287c98e69ce7b4ed15b
Lucas Garcia <lucasugarcia@gmail.com>
2021-11-03 13:54:19 -0300

    Código de exemplo gerando exceção de divisão por zero

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -10,10 +10,27 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(7480, 874150);
-            Console.WriteLine(ContaCorrente.TaxaOperacao);
-
+            Metodo();
             Console.ReadLine();
+        }
+
+        //Teste com a cadeia de chamada:
+        //Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(2);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            return numero / divisor;
         }
     }
 }
