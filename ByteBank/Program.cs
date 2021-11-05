9f7dfacd3079bc353624226aae3dc8e3ec0177a8
Lucas Garcia <lucasugarcia@gmail.com>
2021-11-05 13:53:25 -0300

    Tratando vários tipos de exceção com a classe Exception

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -14,8 +14,13 @@ namespace ByteBank
             {
                 Metodo();
             }
-            catch (NullReferenceException erro)
+            catch (DivideByZeroException erro)
             {
+                Console.WriteLine("Não é possível divisão por zero.");
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine(erro.Message);
                 Console.WriteLine(erro.StackTrace);
                 Console.WriteLine("Aconteceu um erro");
             }
@@ -30,25 +35,16 @@ namespace ByteBank
 
         private static void TestaDivisao(int divisor)
         {
-            try
-            {
-                int resultado = Dividir(10, divisor);
+            int resultado = Dividir(10, divisor);
 
-                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
-            }
-            catch (DivideByZeroException erro)
-            {
-                Console.WriteLine(erro.Message);
-                Console.WriteLine(erro.StackTrace);
-                Console.WriteLine("Não é possível fazer uma divisão por 0!");
-            }
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
         }
 
         private static int Dividir(int numero, int divisor)
         {
             ContaCorrente conta = null;
 
-            Console.WriteLine(conta.Saldo);
+            //Console.WriteLine(conta.Saldo);
 
             return numero / divisor;
         }
