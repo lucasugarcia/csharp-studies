8c8115400f22d99de9ecbb4752178bb75f87356e
Lucas Garcia <lucasugarcia@gmail.com>
2022-02-06 11:34:21 -0300

    Usando regular expression para pegar um número de telefone a partir de um texto

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ByteBank.SistemaAgencia
@@ -11,14 +12,20 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar";
-            ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
+            string padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+            string textoDeTeste = "Meu nome é Lucas, me ligue em 12344321";
 
-            string valor = extratorDeValores.GetValor("moedaDestino");
+            Match resultado = Regex.Match(textoDeTeste, padrao);
 
-            Console.WriteLine("Valor de moedaDestino: " + valor);
-
+            Console.WriteLine(resultado.Value);
             Console.ReadLine();
+
+            //string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar";
+            //ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
+
+            //string valor = extratorDeValores.GetValor("moedaDestino");
+
+            //Console.WriteLine("Valor de moedaDestino: " + valor);
         }
     }
 }
