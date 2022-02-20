d020fc212e5b2ebf9c8fbca24e3691b94556eaac
Lucas Garcia <lucasugarcia@gmail.com>
2022-02-20 09:09:46 -0300

    Lendo arquivo com FileStream e utilizando encoding para escrever o texto

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -1,18 +1,39 @@
 ﻿using ByteBankImportacaoExportacao.Modelos;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ByteBankImportacaoExportacao 
-{ 
-    class Program 
-    { 
-        static void Main(string[] args) 
+namespace ByteBankImportacaoExportacao
+{
+    class Program
+    {
+        static void Main(string[] args)
         {
+            var enderecoArquivo = "contas.txt";
+
+            var fluxoArquivo = new FileStream(enderecoArquivo, FileMode.Open);
+
+            var buffer = new byte[1024];
+            var numeroBytesLidos = -1;
+
+            while (numeroBytesLidos != 0)
+            {
+                numeroBytesLidos = fluxoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
             Console.ReadLine();
         }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            var utf8 = Encoding.Default;
+
+            var texto = utf8.GetString(buffer);
+            Console.Write(texto);
+        }
     }
-} 
- 
+}
